03d740a932407cba5f712778b33612823bbf75e6
boskuv <boris_kv@bk.ru>
2021-11-29 11:41:47 -0800

    add fsm

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,8 +1,39 @@
 class Task < ApplicationRecord
   belongs_to :author, class_name: 'User'
   belongs_to :assignee, class_name: 'User', optional: true
+
   validates :name, presence: true
   validates :description, presence: true
   validates :author, presence: true
   validates :description, length: { maximum: 500 }
+
+  state_machine initial: :new_task do
+    event :to_dev do
+      transition new_task: :in_development
+    end
+
+    event :return_to_dev do
+      transition %i[:in_qa, :in_code_review] => :in_development
+    end
+
+    event :to_qa do
+      transition in_development: :in_qa
+    end
+
+    event :to_code_reveiw do
+      transition in_qa: :in_code_review
+    end
+
+    event :to_pre_release do
+      transition in_code_review: :ready_for_release
+    end
+
+    event :to_released do
+      transition ready_for_release: :released
+    end
+
+    event :to_archived do
+      transition %i[:new_task, :released] => :archived
+    end
+  end
 end
